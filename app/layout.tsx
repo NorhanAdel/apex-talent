f3d8aa5110756f1a241da419939723634b416902
Nourhan Adel <91224846+NorhanAdel@users.noreply.github.com>
2026-03-14 12:00:46 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,22 +9,27 @@ import { useEffect, useState } from "react";
 
 const cairo = Cairo({
   subsets: ["arabic", "latin"],
-  weight: ["300","400","500","600","700","800"],
-  variable: "--font-cairo"
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-cairo",
 });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const [lang, setLang] = useState("en");
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [lang, setLang] = useState<string>("en");
 
   useEffect(() => {
     const savedLang = localStorage.getItem("lang") || "en";
     setLang(savedLang);
+
     document.documentElement.lang = savedLang;
     document.documentElement.dir = savedLang === "ar" ? "rtl" : "ltr";
   }, []);
 
   return (
-    <html>
+    <html lang={lang} dir={lang === "ar" ? "rtl" : "ltr"}>
       <body className={`${cairo.variable} font-sans`}>
         <ThemeProvider>
           <Navbar lang={lang} setLang={setLang} />
