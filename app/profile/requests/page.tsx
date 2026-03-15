52cda5c775f28e1510c4efb2022fb8ce2aa1dc79
Nourhan Adel <91224846+NorhanAdel@users.noreply.github.com>
2026-03-15 14:18:01 +0200

    Update page.tsx

--- app/profile/requests/page.tsx
+++ app/profile/requests/page.tsx
@@ -33,77 +33,49 @@ export default function RequestsPage() {
 
   return (
     <div className="min-h-screen bg-[#020617] flex justify-center py-38 text-white">
-
-      <div className="w-full max-w-6xl  p-10 relative">
-
-        {/* title */}
+      <div className="w-full max-w-6xl p-10 relative">
         <h1 className="text-center text-yellow-400 text-3xl font-bold mb-10">
           Requests
         </h1>
-
-        {/* requests */}
         <div className="flex flex-col gap-8">
-
           {requests.map((req) => (
             <RequestCard key={req.id} req={req} />
           ))}
-
         </div>
-
       </div>
     </div>
   );
 }
 
-
-
-function RequestCard({ req }) {
+function RequestCard({ req }: { req: any }) {
   return (
-
     <div className="border border-[#0d2a5f] bg-[#020d24] p-6 rounded-md">
-
-      {/* header */}
       <div className="flex justify-between items-start mb-4">
-
         <div className="flex items-center gap-4">
-
           <img
             src={req.img}
             className="w-12 h-12 rounded-full object-cover"
           />
-
           <div>
             <h3 className="font-semibold">{req.name}</h3>
             <span className="text-yellow-400 text-sm">{req.role}</span>
           </div>
-
         </div>
-
-        {/* buttons */}
         <div className="flex gap-4">
-
           <button className="px-7 py-1 border-x-3 italic border-red-500 text-red-500 rounded-md hover:bg-red-500 hover:text-white transition ">
             Reject
           </button>
-
           <button className="px-7 py-1 border-x-3 italic bg-[#021448] border-green-500 text-green-500 rounded-md hover:bg-green-500 hover:text-white transition">
             Accept
           </button>
-
         </div>
-
       </div>
-
-      {/* text */}
       <p className="text-gray-300 leading-relaxed mb-4">
         {req.text}
       </p>
-
-      {/* date */}
       <div className="text-right text-sm text-gray-400">
         {req.date}
       </div>
-
     </div>
   );
 }
